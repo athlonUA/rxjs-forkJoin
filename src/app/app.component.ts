e9f414a84d732e588610f564d3c03ce741030d60
Alexander G <alexgarmatenko@gmail.com>
2018-12-19 22:25:35 +0200

    Getting successful results when one inner observable errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { forkJoin, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'forkJoin';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('/api/people/1').subscribe(json => console.log(json));
+
+    const example = forkJoin([
+      this.http.get('https://swapi.co/api/people/1/'),
+      this.http.get('https://swapi.co/api/people/1/'),
+      this.http.get('/api/people/1').pipe(catchError(error => of(error)))
+    ]);
+
+    example.subscribe(val => console.log(val));
+  }
 }
